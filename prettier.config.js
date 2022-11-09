8591528f1e7b423c0761564abe4292f35e25f87b
Theo-Dev <theohamond.dev@gmail.com>
2022-11-09 13:12:37 +0100

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    endOfLine: 'lf',
+}
